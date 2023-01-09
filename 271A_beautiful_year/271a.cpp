d1109d29f746f04b2a366f1465fdd4f234b72431
NguyenVanHuy <87333034+sillycroisant@users.noreply.github.com>
2023-01-09 22:43:04 +0700

    add

--- 271A_beautiful_year/271a.cpp
+++ 271A_beautiful_year/271a.cpp
@@ -1,7 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
+ 
 int main(){
-    int y;
-    cin >> y;
+    system("cls");
+    int year;
+    cin >> year;
+    while(true){
+        year+=1;
+        int a=year/1000;
+        int b=year/100%10;
+        int c=year/10%10;
+        int d=year%10;
+        if(a!=b && a!=c && a!= d && b!=c && b!=d && c!=d){
+            cout<<year;
+            return 0;
+        }
+    }
 }
---
+++ 271A_beautiful_year/271a.py
@@ -0,0 +1,13 @@
+year = int(input())
+while(True):
+    year +=1
+    a = int(year / 1000)
+    b = int(year / 100 % 10)
+    c = int(year / 10 % 10)
+    d = int(year % 10)
+    if a!=b and a!=c and a!=d and b!=c and b!=d and c!=d:
+        print(year)
+        break
+
+
+        
